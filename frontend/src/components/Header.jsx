d44eede09b00a1bdf232043f7a778f7e93965fca
noahmuler <104984522+noahmuler@users.noreply.github.com>
2023-06-22 00:55:43 +0300

    Header Touchup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   };
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="purple" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>ShopHub</Navbar.Brand>
